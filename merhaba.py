d1ac659ba3b3e1870bd55c9faa8912187e75102a
hsyndgzl2834 <adiguzelhuseyin@outlook.com>
2025-05-23 01:11:15 +0300

    güncelleme2

--- merhaba.py
+++ merhaba.py
@@ -1,6 +1,5 @@
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
-rint("GitHub bağlantısı başarıyla çalışıyor!")
-git config --global user.email "adiguzelhuseyin@outlook.com"
-git config --global user.name "hsyndgzl2834"
+
+print("GitHub bağlantısı başarıyla çalışıyor!")
 
