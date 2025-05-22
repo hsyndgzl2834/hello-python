c7e0347e9f4a80e049747a254c34f23ad311207f
Hüseyin Adıgüzel <h.adiguzel@gmail.com>
2025-05-23 00:59:17 +0300

    güncelleme

--- merhaba.py
+++ merhaba.py
@@ -1,1 +1,3 @@
-print("GitHub bağlantısı başarıyla çalışıyor!")
+print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
+print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
+rint("GitHub bağlantısı başarıyla çalışıyor!")
