411674a47bedae837f87fc3ee7d7442dedc404d5
Hüseyin Adıgüzel <h.adiguzel@gmail.com>
2025-05-23 01:02:53 +0300

    güncelleme

--- merhaba.py
+++ merhaba.py
@@ -1,3 +1,6 @@
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 rint("GitHub bağlantısı başarıyla çalışıyor!")
+git config --global user.email "adiguzelhuseyin@outlook.com"
+git config --global user.name "hsyndgzl2834"
+
