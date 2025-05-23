0b77c823e5269778b69e9d83c417c71e922d8617
hsyndgzl2834 <adiguzelhuseyin@outlook.com>
2025-05-23 10:49:12 +0300

    ilk commit

--- merhaba.py
+++ merhaba.py
@@ -1,5 +1,5 @@
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 
-print("GitHub bağlantısı başarıyla çalışıyorson durum basarili !")
+print("GitHub bağlantısı başarıyla çalışıyorson durumjdjhdkshj basarili !")
 
