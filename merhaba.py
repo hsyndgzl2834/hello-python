471f9aebfff5944642270ec9d34ab97191d331aa
hsyndgzl2834 <adiguzelhuseyin@outlook.com>
2025-05-23 10:56:12 +0300

    değişiklik yaptım

--- merhaba.py
+++ merhaba.py
@@ -1,5 +1,5 @@
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
 print("hgfkghghg  GitHub bağlantısı başarıyla çalışıyor!hghgj")
-
 print("GitHub bağlantısı başarıyla çalışıyorson durumjdjhdkshj basarili !")
 
+print("GitHub bağlantısı başarıyla çalışıyorson durumjdjhdkshj basarili !")
